2543d7015a4a7f5cb83d23e3591b921d05ede2f1
egarciaBNA <ezequielgarcia@bna.com.ar>
2020-12-10 15:54:44 -0300

    por las dudas un commit

---
+++ hello-spring/src/main/java/com/login/controller/SampleControlller.java
@@ -0,0 +1,15 @@
+package com.login.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SampleController {
+
+	@RequestMapping("/")
+	public String loadHomePage(Model m) {
+		m.addAttribute("message", "Hello World!");
+		return "index";
+	}
+}
