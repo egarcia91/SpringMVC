4e36a3ddec2381d1f5ab7a92073113959ca7d818
egarcia91 <garciaezequiel91@gmail.com>
2020-12-14 12:39:12 -0300

    Principio de autenticacion Basica

---
+++ pruebaSpringMVC/complete/src/main/java/com/example/servingwebcontent/HttpBasicAuthenticationAdapter.java
@@ -0,0 +1,33 @@
+package com.example.servingwebcontent;
+
+//import org.springframework.security.config.annotation.web.configuration;
+//import org.springframework.security.config.annotation.*;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.config.annotation.authentication.builders.*;
+
+@Configuration
+@EnableWebSecurity
+public class HttpBasicAuthenticationAdapter extends
+WebSecurityConfigurerAdapter {
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth)
+		throws Exception {
+		auth
+			.inMemoryAuthentication()
+			.withUser("userId").password("passwd")
+			.authorities("ROLE_USER");
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+			.antMatchers("/formulario").permitAll()
+			.anyRequest().authenticated()
+			.and()
+			.httpBasic()
+			.realmName("Prueba seguridad");
+	}
+
+}
