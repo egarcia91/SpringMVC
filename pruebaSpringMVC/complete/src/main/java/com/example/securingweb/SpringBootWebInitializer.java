f560692e2ff5a08b3a9486969abe43b978903155
egarcia91 <garciaezequiel91@gmail.com>
2020-12-15 12:20:12 -0300

    vamos a conseguir un WAR!

---
+++ pruebaSpringMVC/complete/src/main/java/com/example/securingweb/SpringBootWebInitializer.java
@@ -0,0 +1,11 @@
+package com.example.securingweb;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+public class SpringBootWebInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SecuringWebApplication.class);
+	}
+}
